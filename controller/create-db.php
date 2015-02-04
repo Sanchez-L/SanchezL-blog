0ab320873d109a5b45ef51d4d1c6cb7bf808075d
leonardo sanchez <leonardodicaprip831@yahoo.ocm>
2015-02-04 08:49:32 -0800

    2/4/15

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,2 +1,13 @@
 <?php
+   require_once("../model/database.php");
 
+    $connection = new mysqli($host, $username, $password);
+    
+    if($connection->connect_error) {
+        die("Error: " . $connection->connect_error);
+    }
+    else{
+        echo "success" . $connection->host_info;
+    }
+    
+    $connection->close();
