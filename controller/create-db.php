c7745456b8903f3326ac419b7affe7fb21cff4fa
leonardo sanchez <leonardodicaprip831@yahoo.ocm>
2015-02-09 18:01:11 -0800

    2/9/15

--- controller/create-db.php
+++ controller/create-db.php
@@ -4,21 +4,34 @@
     $connection = new mysqli($host, $username, $password);
     
     if($connection->connect_error) {
-        die("Error: " . $connection->connect_errors);
+        die("<p>Error: " . $connection->connect_errors> "</p>");
     }
  
     $exists = $connection->select_db($database);    
     
     if(!$exists) {
         
-        $jquery = $connection->query("CREATE DATABASE $database");
+        $query = $connection->query("CREATE DATABASE $database");
         
-         if($jquery) {
-           echo "successfully created database:" . $database;
+         if($query) {
+           echo "<p>successfully created database:" . $database . "</p>";
        }
     } 
     else {
-        echo "database already exists";
+        echo "<p>database already exists</p>";
     }
+    
+    $query = $connection->query("CREATE TABLE posts ("
+            . "id int (11) NOT NULL AUTO_INCREMENT,"
+            . "title varchar(255) NOT NULL,"
+            . "post text NOT NULL,"
+            . "PRIMARY KEY (id))");
      
+    if($query) {
+        echo "<p>successfully created table: posts</p>";
+    }
+    else {
+        echo "<p>$connection->error</p>";
+    }
+    
        $connection->close();
---
+++ view/form.php
@@ -0,0 +1,18 @@
+<h1>Create Blog Post</h1>
+
+<form>
+    <div>
+        <label for="title">Title: </label>
+        <input type="text" name="Title" />
+    </div>
+
+    <div>
+        <label for="post">post: </label>
+        <textarea name="post"></textarea>
+    </div>
+    
+    <div>
+        <button type="submit">submit</button>
+    </div>
+
+</form>
